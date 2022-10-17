8fa8735180d9706a4ef9b8cc34e2c67ff731dd1a
nagamounikag <112828872+nagamounikag@users.noreply.github.com>
2022-10-17 16:55:31 -0400

    Updated my function

--- my-function.js
+++ my-function.js
@@ -1,1 +1,9 @@
-
+exports.handler = async (event) => {
+    JSON.stringify(event)
+    event["queryStringParameters"]["keyword"] = "Mounika Says " + event["queryStringParameters"]["keyword"];
+    const response = {
+        statusCode: 200,
+        body: event
+    };
+    return response;
+};
